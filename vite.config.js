91205c8e338e35936fbc4c88e8c224be6d0cb882
Kristian Duda <kristian.duda@extropy.sk>
2024-07-09 23:28:35 +0200

    eslint

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import path from "node:path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import { name } from "./package.json";
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import { name } from './package.json'
+
+const formattedName = name.match(/[^/]+$/)?.[0] ?? name
 
 export default defineConfig({
   plugins: [dts()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: name,
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: formattedName,
     },
   },
-});
+})
